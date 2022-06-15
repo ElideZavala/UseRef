e9ff12954b99d4b1fab3fbc8f7976daf866e8f23
ElideZavala <zavalavinagreelide@gmail.com>
2022-06-15 09:57:56 -0500

    ScrollAnimation

--- src/ScrollAnimation.js
+++ src/ScrollAnimation.js
@@ -1,12 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 
 const ScrollAnimation = () => {
 	const [background, setBackground] = useState("lightblue");
+	const divRef = useRef();
 
 	useEffect(() => {
 		const handleScroll = () => {
-			console.log('Se hizo scroll')
+			const div = divRef.current;
+			// Metodo hacia el scroll para obtener la posicion del elemento
+			const { y } = div.getBoundingClientRect()
+
+			const backgroundColor = y <= 100 ? 'orange' : 'lightblue';
+			setBackground(backgroundColor);
 		}
 
 		window.addEventListener('scroll', handleScroll);
@@ -20,7 +26,7 @@ const ScrollAnimation = () => {
 
   return (
 	 <div>
-		<div style={{ height: "180vh", background}}>
+		<div ref={divRef} style={{ padding:0, height: "180vh", background}}>
 			<h1>Scroll to change background-color</h1>
 		</div>
 	 </div>
